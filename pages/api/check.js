6eb2053e4fb8ee0aa7c30726c78cfbc1ca465d6a
super-curve-hub <motoki.kajitani@gmail.com>
2026-03-10 20:41:40 +0900

    Update check.js

--- pages/api/check.js
+++ pages/api/check.js
@@ -1,7 +1,50 @@
-export default function handler(req,res){
+import dns from "dns/promises"
 
-res.json({
-status:"ok"
+export default async function handler(req,res){
+
+const {domain} = req.body
+
+let spf=false
+let dmarc=false
+let dkim=false
+let mx=false
+
+try{
+
+const txt = await dns.resolveTxt(domain)
+
+txt.forEach(r=>{
+if(r.join("").includes("v=spf1")) spf=true
 })
+
+}catch{}
+
+try{
+
+const txt = await dns.resolveTxt("_dmarc."+domain)
+
+txt.forEach(r=>{
+if(r.join("").includes("v=DMARC1")) dmarc=true
+})
+
+}catch{}
+
+try{
+
+await dns.resolveTxt("selector1._domainkey."+domain)
+
+dkim=true
+
+}catch{}
+
+try{
+
+await dns.resolveMx(domain)
+
+mx=true
+
+}catch{}
+
+res.json({spf,dkim,dmarc,mx})
 
 }

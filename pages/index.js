3d92bf8703ac9e91cc9826b630fdd3237339d89e
super-curve-hub <motoki.kajitani@gmail.com>
2026-03-10 20:33:36 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+
+  return (
+    <div style={{fontFamily:"sans-serif",padding:40}}>
+      <h1>MailAuth DNS Checker</h1>
+      <p>Deploy成功しました</p>
+    </div>
+  )
+
+}
